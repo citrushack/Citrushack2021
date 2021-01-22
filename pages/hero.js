db2f417df5ef456c2aa60ca8700317cc46665acd
r-jo <rdrsjck+gh@gmail.com>
2021-01-22 14:44:42 -0800

    First asset

--- pages/hero.js
+++ pages/hero.js
@@ -18,7 +18,7 @@ export default function Hero() {
   }, []);
 
   return (
-    <main className={styles.main}>
+    <main className={Herostyles.main}>
       <Particles
         id="tsparticles"
         options={{
