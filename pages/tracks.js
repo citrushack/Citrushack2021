af2ba2bb7ba9d9623937b81db1c0c787f586e522
r-jo <rdrsjck+gh@gmail.com>
2021-01-22 17:43:26 -0800

    Added tracks page

---
+++ pages/tracks.js
@@ -0,0 +1,31 @@
+import styles from "../styles/Common.module.css";
+import Volunteerstyles from "../styles/Volunteer.module.css";
+
+export default function Tracks() {
+  return (
+    <main className={`${styles.main} ${styles.paragraphPadding}`}>
+      <h1 className={styles.title}>Tracks</h1>
+      <p className={`${styles.description}`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas
+        sapien sit amet metus efficitur semper. Integer id luctus enim.
+        Curabitur a arcu et arcu tincidunt lobortis eget sed risus. Suspendisse
+        vitae libero magna. Pellentesque sodales at urna nec rutrum. Nulla lorem
+        nisl, congue id aliquet vitae, malesuada sed lacus. Phasellus dapibus
+        euismod leo, ut scelerisque tellus rhoncus sed.
+      </p>
+      <div className={Volunteerstyles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>Mentors &rarr;</h3>
+        </a>
+
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>Volunteers &rarr;</h3>
+        </a>
+
+        <a href="https://nextjs.org/learn" className={styles.card}>
+          <h3>Learn More &rarr;</h3>
+        </a>
+      </div>
+    </main>
+  );
+}
