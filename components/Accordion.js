b19a5200abb629fe15ab1cb84324704add2a3d43
codejeet <ajeetkokatay@gmail.com>
2021-01-24 16:30:31 -0800

    mobile styles

--- components/Accordion.js
+++ components/Accordion.js
@@ -59,23 +59,23 @@ allowZeroExpanded
       answer="Email us at citrushack@gmail.com and we can answer your questions!"
       />
       <FaqSection
-      question="* What if I’ve never been to a hackathon before?"
+      question="What if I’ve never been to a hackathon before?"
       answer="That’s the best reason to come out! Citrus Hack is open to people of all skill-levels! We’ll be having beginner-oriented workshops and activities, along with mentors to help you turn your project into reality!"
       />
       <FaqSection
-      question="* What will I need to participate?"
+      question="What will I need to participate?"
       answer="A working device (i.e your laptop or PC) and a stable internet connection. And, of course, you!"
       />
       <FaqSection
-      question="* What if I don’t have a team?"
+      question="What if I don’t have a team?"
       answer="If you don’t have a team formed prior to Citrus Hack, we will be having a channel on Discord where you can find other participants that share similar interests! We’ll also be providing activities at the beginning of the event to help you form teams! Teams aren’t required though, so feel free to work individually."
       />
       <FaqSection
-      question="* Do I have to hack or make a project?"
+      question="Do I have to hack or make a project?"
       answer="Not at all! We’ll be hosting workshops for you to learn from, events and activities to help you network with others, and games for you to meet other hackers. Don’t forget about the raffles! You’ll be able to find the schedule on our website as we get closer to the event. "
       />
       <FaqSection
-      question="* If I’m residing outside of the U.S, can I still attend and/or win a prize/raffle?"
+      question="If I’m residing outside of the U.S, can I still attend and/or win a prize/raffle?"
       answer="If you’re residing outside of the U.S, you can still attend! However, due to shipping restrictions and COVID, we won’t be able to send any type of physical prizes,  swag items, or raffle prizes to those residing outside of the U.S. You are still eligible to win a category, however!
       If you currently attend a university within the U.S but reside outside of the U.S, you’ll be able to win a category but will not be eligible to receive a physical prize, swag item, or raffle prize. 
       If you both attend a university outside of the U.S and also reside outside of the U.S, you will e ineligible to win or receive a prize, but are still encouraged to attend and participate in all festivities!
