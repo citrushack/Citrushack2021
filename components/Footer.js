abacb3791b458bcb9a6536d970fe90bc93a36ddb
claynaut <jpesc002@ucr.edu>
2021-01-25 23:06:27 -0800

    Text is now the link to the team page, pointer noy only appears above links

--- components/Footer.js
+++ components/Footer.js
@@ -4,19 +4,18 @@ import {FaEnvelope, FaFacebook, FaTwitter, FaInstagram} from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <Link href="/team">
-      
-      <a>
-        <footer className={styles.footer}>
-          <div className ={styles.contact}>
-            <a href="mailto:citrushack@gmail.com"><FaEnvelope /></a>
-            <a href="https://www.facebook.com/CitrusHack/"><FaFacebook/></a>
-            <a href="https://twitter.com/citrushack?lang=en"><FaTwitter/></a>
-            <a href="https://www.instagram.com/citrushack_ucr/?hl=en"><FaInstagram/></a>
-          </div>
-          <p>Powered by love and coffee from the Citrushack team.</p>
-        </footer>
-      </a>
-    </Link>
+    <div className={styles.footer}>
+      <div className ={styles.contact}>
+        <a href="mailto:citrushack@gmail.com"><FaEnvelope /></a>
+        <a href="https://www.facebook.com/CitrusHack/"><FaFacebook/></a>
+        <a href="https://twitter.com/citrushack?lang=en"><FaTwitter/></a>
+        <a href="https://www.instagram.com/citrushack_ucr/?hl=en"><FaInstagram/></a>
+      </div>
+      <div className={styles.teamlink}>
+        <a href="/team">
+          Powered by love and coffee from the Citrushack team.
+        </a>
+      </div>
+    </div>
   );
 }
