87f9724c461c3604438d4c8f0faa9a6a4de1aca0
codejeet <ajeetkokatay@gmail.com>
2021-01-30 22:53:49 -0800

    fixed stuck track animation

--- pages/tracks.js
+++ pages/tracks.js
@@ -9,19 +9,17 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 const variants = {
   enter: (direction) => {
     return {
-      x: direction > 0 ? 100 : -100,
+      x: direction > 0 ? 1000 : -1000,
       opacity: 0
     };
   },
   center: {
-    zIndex: 1,
     x: 0,
     opacity: 1
   },
   exit: (direction) => {
     return {
-      zIndex: -1,
-      x: direction < 0 ? 100 : -100,
+      x: direction < 0 ? 1000 : -1000,
       opacity: 0
     };
   }
