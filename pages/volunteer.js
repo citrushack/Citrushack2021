ff638c94dbca48d9edb6b63c413ae964c61034cf
codejeet <ajeetkokatay@gmail.com>
2021-01-31 00:22:18 -0800

    button style tweaks

--- pages/volunteer.js
+++ pages/volunteer.js
@@ -1,24 +1,24 @@
 import styles from "../styles/Common.module.css";
-import Volunteerstyles from "../styles/Volunteer.module.css";
+import vs from "../styles/Volunteer.module.css";
 import { FaChevronRight } from "react-icons/fa";
 
 export default function Volunteer() {
   return (
     <main className={styles.main}>
-      <div className={Volunteerstyles.wrapper}>
+      <div className={vs.wrapper}>
         <h1 className={styles.title}>Get Involved</h1>
-        <p className={`${styles.description} ${Volunteerstyles.pad}`}>
+        <p className={`${styles.description} ${vs.pad}`}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas
           sapien sit amet metus efficitur semper.
         </p>
         <div className={styles.grid}>
-          <a href="https://forms.gle/fCtHkDG7favDxifF6" className={styles.card}>
+          <a href="https://forms.gle/fCtHkDG7favDxifF6" className={`${styles.card} ${vs.color1}`}>
             <h3>
               Mentors <FaChevronRight className={styles.icon} />
             </h3>
           </a>
 
-          <a href="https://forms.gle/ewWRkKgzq4q9Zhok6" className={styles.card}>
+          <a href="https://forms.gle/ewWRkKgzq4q9Zhok6" className={`${styles.card} ${vs.color2}`}>
             <h3>
               Volunteers <FaChevronRight className={styles.icon} />
             </h3>
