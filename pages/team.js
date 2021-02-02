063524488c5e9ae57435949a95106e08ff42e69d
codejeet <ajeetkokatay@gmail.com>
2021-02-02 04:36:23 -0800

    committe

--- pages/team.js
+++ pages/team.js
@@ -205,6 +205,7 @@ export default function Team() {
             <h3>Web Dev Committee</h3>
             <ul>
               <li>J.S. Pescasio</li>
+              <li>Andrei Dimaano</li>
             </ul>
           </div>
         </div>
