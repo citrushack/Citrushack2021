63606cdd28bfa97e43c655e84e2ce116375364fc
r-jo <rdrsjck@gmail.com>
2021-03-22 08:21:19 -0700

    Add judges

--- pages/live.js
+++ pages/live.js
@@ -1,39 +1,28 @@
 import React from "react";
+import Image from "next/image";
 import Container from "../components/Container";
-import Countdown from "react-countdown";
+import CountdownTimer from "../components/Countdown";
 
 import Livestyles from "../styles/Live.module.css";
 import styles from "../styles/Common.module.css";
 
-// Random component
-const Completionist = () => <h2>Hacking has ended!</h2>;
-
-// Renderer callback with condition
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  if (completed) {
-    // Render a completed state
-    return <Completionist />;
-  } else {
-    // Render a countdown
-    return (
-      <h2>
-        {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds
-        left!
-        <br />
-        <br />
-        {days}:{hours}:{minutes}:{seconds} left!
-      </h2>
-    );
-  }
+const Judge = ({ picture, username, title }) => {
+  return (
+    <div className="Livestyles.judge">
+      <Image src={picture} alt="Judge profile picture" width={100} height={100}/>
+      <h3>{username}</h3>
+      <p>{title}</p>
+    </div>
+  );
 };
 
 export default function Live() {
   return (
     <Container main>
       <div className={styles.container}>
-        <h1>Live Page</h1>
-        <Countdown date={"2021-04-11T24:00:00"} renderer={renderer} />
+        <CountdownTimer />
         <h2>Judges</h2>
+        <Judge picture="/logoRevised.png" username="Scotty" title="Mascot" />
         <h2>Schedule</h2>
         <h2>Resources</h2>
         <h2>Sponsors</h2>
